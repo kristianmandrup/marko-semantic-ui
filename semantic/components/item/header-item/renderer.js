e82342db092221af2c04805c4607f78c9e4762ca
Kristian Mandrup <kmandrup@gmail.com>
2015-09-05 20:57:31 +0100

    menus

--- semantic/components/item/header-item/renderer.js
+++ semantic/components/item/header-item/renderer.js
@@ -1,5 +1,6 @@
 var template = require('./template.marko');
 
 exports.renderer = function(input, out) {
-  template.render(input, out);
+    input.tagName = data.link ? 'a' : 'div';
+    template.render(input, out);
 };
